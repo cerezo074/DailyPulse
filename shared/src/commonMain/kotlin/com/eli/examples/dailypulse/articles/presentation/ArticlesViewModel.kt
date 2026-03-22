de44e0aab3f88ba02573c2418497044cee345635
Eli Pacheco Hoyos <eph_074@hotmail.com>
2026-03-23 00:01:37 +0100

    Fix cancellation for iOS asyn refresh operator

--- shared/src/commonMain/kotlin/com/eli/examples/dailypulse/articles/presentation/ArticlesViewModel.kt
+++ shared/src/commonMain/kotlin/com/eli/examples/dailypulse/articles/presentation/ArticlesViewModel.kt
@@ -6,7 +6,9 @@ import com.rickclephas.kmp.nativecoroutines.NativeCoroutinesState
 import com.rickclephas.kmp.observableviewmodel.ViewModel
 import com.rickclephas.kmp.observableviewmodel.MutableStateFlow
 import com.rickclephas.kmp.observableviewmodel.launch
+import com.rickclephas.kmp.observableviewmodel.stateIn
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
 
@@ -16,34 +18,43 @@ class ArticlesViewModel(
 
     private val _contentState = MutableStateFlow(viewModelScope, ArticlesState(loading = true))
     private val _isRefreshing = MutableStateFlow(viewModelScope, false)
+
     @NativeCoroutinesState
     val contentState: StateFlow<ArticlesState> = _contentState
+
     @NativeCoroutinesState
     val isRefreshing: StateFlow<Boolean> = _isRefreshing
 
     init {
         viewModelScope.launch {
             val fetchedArticles = listArticleUseCase.getArticles(false)
-            _contentState.update{ (ArticlesState(articles = fetchedArticles, loading = false)) }
+            _contentState.update { (ArticlesState(articles = fetchedArticles, loading = false)) }
         }
     }
 
     fun onRefreshContent() {
         viewModelScope.launch {
-            refreshContent()
+            refreshContent(useSuspension = false)
         }
     }
 
     @NativeCoroutines
     suspend fun onRefreshContentAsync() {
-        refreshContent()
+        refreshContent(useSuspension = true)
     }
 
-    private suspend fun refreshContent() {
-        _isRefreshing.update { true }
+    private suspend fun refreshContent(useSuspension: Boolean) {
+        if (!useSuspension) {
+            _isRefreshing.update { true }
+        }
+
         delay(5000)
         val fetchedArticles = listArticleUseCase.getArticles(true)
-        _isRefreshing.update { false }
+
+        if (!useSuspension) {
+            _isRefreshing.update { false }
+        }
+
         _contentState.update { ArticlesState(articles = fetchedArticles) }
     }
 }
